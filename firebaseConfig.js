c5a573191a5d3a9274846300af7ba658c6b3fbe9
adachi-shu <backnumber0620@icloud.com>
2024-11-26 13:29:04 +0900

    add firebase sdk

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAG3uUuMxH14nOpaj5c8Ae6ao6xa-vNi1k",
+  authDomain: "golf-score-app-76ca7.firebaseapp.com",
+  projectId: "golf-score-app-76ca7",
+  storageBucket: "golf-score-app-76ca7.firebasestorage.app",
+  messagingSenderId: "4318045569",
+  appId: "1:4318045569:web:268396cab62bfc9e9a7480",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
